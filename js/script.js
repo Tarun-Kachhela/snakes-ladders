8087324eaba9ba1cb2353a76a566d08392f42fc0
DELL <tarun.kachhela2000@gmail.com>
2019-08-27 22:01:39 +0530

    core running

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(function(){
+//    $(".goti").click(function(){
+//        $(this).hide();
+//    });
+//    document.getElementById("CreateGoti").addEventListener("click", createGoti);
+   randomNumberGenerator();
+});
+
+function createGoti(){
+    alert("hell");
+    
+}
+function randomNumberGenerator(){
+    alert(Math.random(0,6));
+}

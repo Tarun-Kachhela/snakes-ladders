db112cf27d341fd9f2db21dc875209d45ec396f2
DELL <tarun.kachhela2000@gmail.com>
2019-08-30 18:48:02 +0530

    core work done

--- js/script.js
+++ js/script.js
@@ -1,23 +1,61 @@
 $(function(){
-    i=0;
+    i=0,random=0,whichPlayer=0,previousPosition=0;
     var playersPositions = new Map();
+    var laddersMapper=mapper([[5,35],[17,43],[12,78],[33,67],[65,95]]);
+    var snakesMapper=mapper([[32,8],[41,18],[66,36],[99,56],[87,69]]);
+    var rankList = mapper()
+    
     $(".ButtonForRandomNumber").click(function(){
-        randomNumberGenerator();
+        $("#CreateGoti").hide();
+        random=randomNumberGenerator();
+        playerPosition =playersPositions.get(whichPlayer)+random;
+        if(laddersMapper.has(playerPosition)){
+            playerPosition= laddersMapper.get(playerPosition);
+            previousPosition=playersPositions.get(whichPlayer);
+            alert(previousPosition);
+            $(".boxes #goti"+previousPosition).remove();
+            $(".boxes #box"+playerPosition).html("<div class =\"goti\" id=\"goti"+whichPlayer+"\">"+whichPlayer+"</div>");
+            $(".boxes #box"+playerPosition+" #goti"+whichPlayer).css("display","block");
+        }
+        else if(snakesMapper.has(playerPosition)){
+            playerPosition = snakesMapper.get(playerPosition);
+            previousPosition=playersPositions.get(whichPlayer);
+            alert(previousPosition);
+            $(".boxes #goti"+previousPosition).remove();
+            $(".boxes #box"+playerPosition).html("<div class =\"goti\" id=\"goti"+whichPlayer+"\">"+whichPlayer+"</div>");
+            $(".boxes #box"+playerPosition+" #goti"+whichPlayer).css("display","block");
+        }
+        if(playerPosition == 100){
+            playersPositions.delete(whichPlayer);
+            previousPosition=playersPositions.get(whichPlayer);
+            alert(previousPosition);
+            $(".boxes #goti"+previousPosition).remove();
+            $(".boxes #box"+playerPosition).html("<div class =\"goti\" id=\"goti"+whichPlayer+"\">"+whichPlayer+"</div>");
+            $(".boxes #box"+playerPosition+" #goti"+whichPlayer).css("display","block");
+//            alert(playersPositions.get(0) +" "+playersPositions.get(1) +" "+whichPlayer+" "+random);
+        }
+        else if(playerPosition<=100){
+            playersPositions.set(whichPlayer,playerPosition);
+            previousPosition=playersPositions.get(whichPlayer);
+            alert(previousPosition);
+            $(".boxes #goti"+previousPosition).remove();
+            $(".boxes #box"+playerPosition).html("<div class =\"goti\" id=\"goti"+whichPlayer+"\">"+whichPlayer+"</div>");
+            $(".boxes #box"+playerPosition+" #goti"+whichPlayer).css("display","block");
+//            alert(playersPositions.get(0) +" "+playersPositions.get(1) +" "+whichPlayer+" "+random);
+        }
+        if((playersPositions.size-1)==whichPlayer)
+            whichPlayer=0;
+        else
+            whichPlayer++;
     });
     $("#CreateGoti").click(function(){
         createGoti();
     });
 //    document.getElementById("CreateGoti").addEventListener("click", createGoti);
-    random=randomNumberGenerator();
-    var addersMapper=mapper([[5,35],[17,43],[12,78],[33,67],[65,95]]);
-    var snakesMapper=mapper([[32,8],[41,18],[66,36],[99,56],[87,69]]);
-//    createGoti([["tarun",0],["pank",0]]);
     function createGoti(){
-//    alert("hell");
-    //create goti design
-        $(".row1 #box1").append("<div class=\"goti\">"+i+"</div>");
-        $(".row1 #box1 .goti").css("display","block");
-        playersPositions.push();
+        $(".row1 #box1").append("<div class =\"goti\" id=\"goti"+i+"\">"+i+"</div>");
+        $(".row1 #box1 #goti"+i).css("display","block");
+//        playersPositions.push();
         playersPositions.set(i,0);
         i++;
     }
@@ -29,5 +67,7 @@ function mapper(mapperingData){
 
 function randomNumberGenerator(){
 //    alert(Math.floor(Math.random() * 6)+1);
-    return $(".RandomNumber").html( Math.floor(Math.random() * 6)+1);
+    newNumber = Math.floor(Math.random() * 6)+1;
+    $(".RightContainer .RandomNumber").html(newNumber);
+    return newNumber;
 }
